bb4279dfe0a39b7a322f2d4929901e826b725076
uvsqq21916400 <flaviekutukialanda@gmail.com>
2026-02-12 21:43:01 +0100

    Modifs

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
     server: {
         proxy: {
             '/api': 'http://127.0.0.1:8080',
-            '/management': 'http://127.0.0.1:8080',
+            '/management': 'http://127.0.0.1:8082',
         },
     },
 });
